804836d9ed95b93ebed8d591ac127de7a839d425
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2017-04-18 10:08:04 +0200

    Some more changes

--- Scripts/activation_functions.py
+++ Scripts/activation_functions.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 x = np.arange(-2,2,0.01)
 sigmoid = np.exp(x)/(1 + np.exp(x))
 tanh = (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
-relu = np.maximum(x, 0)
-lrelu = np.append(0.333 * x[x<0], x[x>=0])
+relu = np.maximum(x-0.01, 0)
+cut_off = 0.1
+
+a = 0.333 * x[x<0]
+b = x[x>=0] + 0.01
+lrelu = np.append(a, b)
 
 plt.rc('text', usetex=True)
 plt.rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern Roman']})
 
 #plt.rc('font', family='serif')
 
-plt.plot(x,sigmoid, x, tanh, x, relu, x, lrelu)
+plt.plot(x,sigmoid, x, tanh, x, relu, x, lrelu, linewidth='2')
 plt.xlabel(r'x')
 plt.ylabel(r'\textit{voltage} (mV)',fontsize=16)
 plt.title(r'Activation functions')
-plt.legend([r'sigmoid', "hyperbolic tangent", "rectifier", "leaky rectifier"], loc=2)
+plt.legend([r'sigmoid', "hyperbolic tangent", "rectifier", "leaky rectifier ($\\alpha=0.333$)"], loc=2)
 
 plt.savefig('../figs/activations.pdf')
 
